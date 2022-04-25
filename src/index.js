e0a526af427dfa8dd3b4b1afaf4397ad88338dd2
Stas Remnev <remnev.stas@gmail.com>
2022-04-25 16:11:02 +0300

    style: structure

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { render } from 'react-dom';
+
+function App() {
+  return (
+    <>
+      <h1> hello yop </h1>
+    </>
+  );
+}
+
+render(<App />, document.querySelector('#root'));
